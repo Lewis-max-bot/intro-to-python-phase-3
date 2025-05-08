7347f9aaa7e1ec8f1392c4cd755ed495bb323eef
Lewis <lewis.njuma@student.moringaschool.com>
2025-05-08 11:25:45 +0300

    added changes

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 // declaring a variable in javascript 
 
-// const name = "Lynne"
+const name = "Lynne"
 
-// console.log(name);
+console.log(name);
 
 // writing functions in js
 
-// function sayHi() {
-//     console.log("wozza, gang!");
-// }
-// sayHi()
-// const age = 17;
+function sayHi() {
+    console.log("wozza, gang!");
+}
+sayHi()
+const age = 17;
 
-// if (age < 18) {
-//     console.log("A minoooooor!");
-// }else if (age > 18) {
-//     console.log("I love K Dot");
-// }
+if (age < 18) {
+    console.log("A minoooooor!");
+}else if (age > 18) {
+    console.log("I love K Dot");
+}
+
+// use camel case in frontend development
+
+// multiple declaration of variables in javascript
+
+const [a, b, c] = ["Andrew", "Tristan","Camila"];
+console.log(c)
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # in python we dont use let, var or const to declare variables , instead we say
 
-# name = "Lynne"
+name = "Lynne"
 # in python we dont use console.log, instead we use PRINT 
 
-# print(name);
+print(name);
 
 # writing functions in python 
 
@@ -15,3 +15,28 @@ say_hi()
 
 # curly braces are not used in python 
 # use snake case in python(backend development)
+
+age = 13;
+
+if age < 18:
+    print("A minooooooor!")
+elif age > 18:
+    print("I love K Dot")
+
+# else if statement in javascript is replaced with "elif"
+
+# it can be written as 
+
+print("BBL Drizzy") if age < 18 else print("I love Kendrick")
+
+# arrays in javascript are called "LISTS" in python 
+# LISTS
+
+student = {"name": "Alex", "age": 39, "preferred_language": "python"}
+print(student)
+
+# variables in python
+# multiple declaration
+
+a, b, c = "Ann", "Collins", "Lewis";
+print(c)
