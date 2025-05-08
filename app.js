a6a841ecf7566a00afb43b3708bd3323cefb4a10
Lewis <lewis.njuma@student.moringaschool.com>
2025-05-08 10:31:17 +0300

    intro to python

---
+++ app.js
@@ -0,0 +1,19 @@
+// declaring a variable in javascript 
+
+// const name = "Lynne"
+
+// console.log(name);
+
+// writing functions in js
+
+// function sayHi() {
+//     console.log("wozza, gang!");
+// }
+// sayHi()
+// const age = 17;
+
+// if (age < 18) {
+//     console.log("A minoooooor!");
+// }else if (age > 18) {
+//     console.log("I love K Dot");
+// }
---
+++ app.py
@@ -0,0 +1,17 @@
+# in python we dont use let, var or const to declare variables , instead we say
+
+# name = "Lynne"
+# in python we dont use console.log, instead we use PRINT 
+
+# print(name);
+
+# writing functions in python 
+
+def say_hi():
+ print("Wozza, gang")
+ print("Tito is the one who threw the shoe!")
+
+say_hi()
+
+# curly braces are not used in python 
+# use snake case in python(backend development)
